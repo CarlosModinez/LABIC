49d6685a314284cff1ccb50532ec5d43a9ab30b2
Carlos Modinez <modinez.carlos@gmail.com>
2020-06-30 09:23:21 -0300

    Exercicio 3 modificado

--- Aulas python/Exercicio_3.py
+++ Aulas python/Exercicio_3.py
@@ -1,12 +1,21 @@
 def gerar_multiplos_de_tres(numero_inicial, numero_final):
     lista = []
-    for i in range (numero_inicial, numero_final, 3):
-        if i != 0:
-            lista.append(i)
+    numero_inicial_da_lista = numero_inicial
+
+    # Obtem o menor numero multiplo de 3 que vai na lista
+    while True:
+        if numero_inicial_da_lista % 3 == 0 and numero_inicial_da_lista != 0:
+            break
+        else:
+            numero_inicial_da_lista += 1
+
+    # Monta a lista a parir do menor multiplo de 3 entre o numero_inicial e o numero_final
+    for i in range (numero_inicial_da_lista, numero_final, 3):
+        lista.append(i)
     return lista
 
 
-lista = gerar_multiplos_de_tres(0, 50)
+lista = gerar_multiplos_de_tres(1, 50)
 
 print("Tamanho da lista: %d" % len(lista))
 print("Maior valor: %d" % max(lista))

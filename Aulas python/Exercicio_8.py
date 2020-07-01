5df40045f1487838109b76eb9cbc577ed25fc086
Carlos Modinez <modinez.carlos@gmail.com>
2020-07-01 12:02:38 -0300

    Exercicio 8 modificado

--- Aulas python/Exercicio_8.py
+++ Aulas python/Exercicio_8.py
@@ -5,4 +5,4 @@ def determinar_volume_inicial(volume_final, numero_de_dias):
 numero_de_dias = int(input("Número de dias: "))
 volume_final = float(input("Volume final da cultura: "))
  
-print("Volume incial necessário: %.2f" % determinar_volume_inicial(volume_final, numero_de_dias))
+print("Volume incial necessário: %.4f" % determinar_volume_inicial(volume_final, numero_de_dias))
